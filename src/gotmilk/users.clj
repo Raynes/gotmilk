6a30c35ace932fb7da43c4b3593fae9634904bd6
Raynes <DiscipleRayne@gmail.com>
2010-07-06 08:14:33 -0500

    Added a search-users function.

--- src/gotmilk/users.clj
+++ src/gotmilk/users.clj
@@ -14,3 +14,14 @@
 (defcommand "followers"
   "Get a list of a user's followers."
   [user] (-> user show-followers format-result))
+
+(defcommand "following"
+  "Get a list of users that a user is following."
+  [user] (-> user show-following format-result))
+
+(defcommand "search-users"
+  "Search for users on github. First argument should be the maximum number of results
+to return"
+  [n & query] (->> query (interpose " ") (apply str) search-users
+                   (take (Integer/parseInt n)) (map format-result)
+                   (interpose "\n") (apply str))) 
