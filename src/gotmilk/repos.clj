c61d30bc523dd3ee21876a3002afdc1a24df8db2
Raynes <DiscipleRayne@gmail.com>
2010-07-09 16:07:11 -0500

    Combined all the commands into a single command with a fuckton of options.

--- src/gotmilk/repos.clj
+++ src/gotmilk/repos.clj
@@ -3,38 +3,37 @@
         clj-github.repos
         [clojure.contrib.string :only [join]]))
 
-(defcommand "collaborator"
-  "Add or remove a collaborator. Use --add to add a collaborator or --rm
-to remove one. Will default to --add, values taken are repo and user."
-  [repo user]
-  (-> ((if (option? options :rm)
-         remove-collaborator
-         add-collaborator)
-       user repo)
-      format-result))
-
-(defcommand "deploy-key"
-  "Add or remove a deploy key. Use --add to add a key, or --rm to remove one.
-Will default to --add. Values taken are repo, title, and key or repo and id (title)
-if you are removing a key."
-  [repo title key]
-  (-> (if (option? options :rm)
-        (remove-deploy-key repo title)
-        (add-deploy-key repo title key))
-      format-result))
-
 (defcommand "repo"
-  "Create or delete a repo.
+  "Create, delete, fork, a repo. Also for adding and removing collaborators and deploy keys.
 for creation (--create) : The first argument is required, and it should be the name of the repo.
 Optional following keys are as follows, in order: description, homepage, and whether or
 not the repo is public which should be true or false.
 
-for deletion (--delete): Just supply the name of the repo."
+for deletion (--delete): Just supply the name of the repo.
+
+for forking (--fork): Just supply the name of the repo, and the user that owns the repo you want
+to fork, in that order.
+
+for adding a collaborator (--add-collaborator): Supply the name of the repo and the user you want to add.
+
+for removing a collaborator (--remove-collaborator): Same as for adding one.
+
+for adding a deploy key (--add-deploy): Supply repo, key title, and the key contents.
+
+for removing a deploy key (--remove-deploy): Supply repo name and title of the key.
+
+Will default to --create"
   [name & [desc home pub]]
-  (if (option? options :delete)
-    (-> name delete-repo format-result)
-    (-> (create-repo name
-         :description desc
-         :homepage home
-         :public (or (= pub "true") (nil? pub)))
-        format-result)))
+  (cond-options
+   options
+   :delete (-> name delete-repo format-result)
+   :fork (-> (fork-repo desc name) format-result)
+   :add-collaborator (-> (add-collaborator desc name) format-result)
+   :remove-collaborator (-> (remove-collaborator desc name) format-result)
+   :add-deploy (-> (add-deploy-key name desc home))
+   :remove-deploy (-> (remove-deploy-key name desc))
+   :else (-> (create-repo name
+                          :description desc
+                          :homepage home
+                          :public (or (= pub "true") (nil? pub)))
+             format-result)))
