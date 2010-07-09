9b34a2d94685f2f2a40b89dfa38f267053537e13
Raynes <DiscipleRayne@gmail.com>
2010-07-09 16:58:01 -0500

    Added repo searching.

--- src/gotmilk/repos.clj
+++ src/gotmilk/repos.clj
@@ -4,8 +4,7 @@
         [clojure.contrib.string :only [join]]))
 
 (defcommand "repo"
-  "Create, delete, fork, a repo. Also for adding and removing collaborators and deploy keys.
-for creation (--create) : The first argument is required, and it should be the name of the repo.
+  "for creation (--create) : The first argument is required, and it should be the name of the repo.
 Optional following keys are as follows, in order: description, homepage, and whether or
 not the repo is public which should be true or false.
 
@@ -22,6 +21,10 @@ for adding a deploy key (--add-deploy): Supply repo, key title, and the key cont
 
 for removing a deploy key (--remove-deploy): Supply repo name and title of the key.
 
+for searching (--search): Supply the number of results you want to return and a string with your
+search terms. Optionally, supply --language=<language> and --start-page=<start-page> to narrow your
+search.
+
 Will default to --create"
   [name & [desc home pub]]
   (cond-options
@@ -32,6 +35,8 @@ Will default to --create"
    :remove-collaborator (-> (remove-collaborator desc name) format-result)
    :add-deploy (-> (add-deploy-key name desc home))
    :remove-deploy (-> (remove-deploy-key name desc))
+   :search (->> (apply search-repos desc (apply concat (dissoc options :search)))
+                (take (Integer/parseInt name)) (map format-result) (apply str))
    :else (-> (create-repo name
                           :description desc
                           :homepage home
