796493b259be841358883f373db723bed5ae4cd5
Raynes <DiscipleRayne@gmail.com>
2010-07-10 12:55:53 -0500

    I believe the repo command is finished. And it's awesome. It rocks your world.

--- src/gotmilk/repos.clj
+++ src/gotmilk/repos.clj
@@ -6,12 +6,13 @@
 (defcommand "repo"
   "for creation (--create) : The first argument is required, and it should be the name of the repo.
 Optional following keys are as follows, in order: description, homepage, and whether or
+
 not the repo is public which should be true or false.
 
 for deletion (--delete): Just supply the name of the repo.
 
-for forking (--fork): Just supply the name of the repo, and the user that owns the repo you want
-to fork, in that order.
+for forking (--fork): Just supply the name of the user that owns the repo, and the name of the repo,
+in that order.
 
 for adding a collaborator (--add-collaborator): Supply the name of the repo and the user you want to add.
 
@@ -21,22 +22,71 @@ for adding a deploy key (--add-deploy): Supply repo, key title, and the key cont
 
 for removing a deploy key (--remove-deploy): Supply repo name and title of the key.
 
-for searching (--search): Supply the number of results you want to return and a string with your
-search terms. Optionally, supply --language=<language> and --start-page=<start-page> to narrow your
-search.
+for searching (--search): Supply a string with your search terms. Optionally, supply --language=<language>
+and --start-page=<start-page> to narrow your search. Also optionally supply --results=<number> to limit the
+number of results gotmilk gives you.
+
+for setting info about a repo (--set-info): Possible keys are description, homepage, has_wiki,
+has_downloads, and has_issues. Supply the username of the owner of the repo, the repo name, a key,
+and that key's values.
+
+for setting repo visibility (--set-visibility): Supply the name of the repo and either 'public' or
+'private'.
+
+for watching a repo (--watch): Supply the user that owns the repo, and the name of the repo.
+
+for unwatching a repo (--unwatch): Same as for watching one.
+
+for showing the collaborators on a project (--show-collaborators): Supply the user that owns the repo,
+and the name of the repo.
+
+for showing the contributors to a project (--show-contributors): Supply the name of the users that owns it,
+the name of the repo, and optionally --anon if you'd like to include anonymous contributors in your results.
+Also optionally supply --results=<number> to limit the number of results printed.
+
+for showing the deploy keys a project uses (--show-deploy-keys): Supply the name of the repo.
+
+for showing the languages used by a project (--show-languages): Same as for --show-collaborators.
+
+for showing a repo's network (--show-network): Same as for --show-collaborators. Also, optionally supply
+--results=<number> to limit the number of results.
+
+for getting a list of repos you can push to (--show-pushable): Optionally supply --results=<number> to
+limit the number of results.
+
+for showing detailed information about a repo (--show-repo-info): Same as for --show-collaborators.
+
+for showing all the repos a user has (--show-repos): Supply the username. Optionally supply --results=<number>
+to limit the number of results.
+
+for showing a repo's tags (--show-tags): Supply the name of the user who owns the repo and the repo's name.
 
 Will default to --create"
   [name & [desc home pub]]
   (cond-options
    options
    :delete (-> name delete-repo format-result)
-   :fork (-> (fork-repo desc name) format-result)
+   :fork (-> (fork-repo name desc) format-result)
    :add-collaborator (-> (add-collaborator desc name) format-result)
    :remove-collaborator (-> (remove-collaborator desc name) format-result)
    :add-deploy (-> (add-deploy-key name desc home))
    :remove-deploy (-> (remove-deploy-key name desc))
-   :search (->> (apply search-repos desc (apply concat (dissoc options :search)))
-                (take (Integer/parseInt name)) (map format-result) (apply str))
+   :search (-> (apply search-repos name (apply concat (dissoc options :search)))
+               (take-and-format (:results options)))
+   :set-info (-> (set-repo-info name desc home pub) format-result)
+   :set-visibility (-> (set-repo-visibility name desc) format-result)
+   :watch (-> (watch-repo name desc) format-result)
+   :unwatch (-> (unwatch-repo name desc) format-result)
+   :show-collaborators (-> (show-collaborators name desc) format-result)
+   :show-contributors (-> (show-contributors name desc :include-anon? (option? options :anon))
+                          (take-and-format (:results options)))
+   :show-deploy-keys (-> (show-deploy-keys name) format-result)
+   :show-languages (-> (show-languages name desc) format-result)
+   :show-network (-> (show-network name desc) (take-and-format (:results options)))
+   :show-pushable (-> (show-pushable) (take-and-format (:results options)))
+   :show-repo-info  (-> (show-repo-info name desc) format-result)
+   :show-repos (-> (show-repos name) (take-and-format (:results options)))
+   :show-tags (-> (show-tags name desc) format-result)
    :else (-> (create-repo name
                           :description desc
                           :homepage home
